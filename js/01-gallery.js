3b65012de37cda147efe85c10c2b527f735e2f33
GusarovaYuliya <93098363+GusarovaYuliya@users.noreply.github.com>
2023-08-20 22:08:38 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,8 +3,8 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 const listEl = document.querySelector(".gallery");
-const renderList =  galleryItems.map(
-  (image) =>  `<li class="gallery_item">
+const renderList = galleryItems.map(
+  (item) => `<li class="gallery_item">
   <a class="gallery_link" href="${item.original}">
   <img
   class="gallery_image"
