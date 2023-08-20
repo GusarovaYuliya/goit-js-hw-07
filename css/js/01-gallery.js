ee69755ae28d735ef78bcdfc9c396e783f077462
GusarovaYuliya <93098363+GusarovaYuliya@users.noreply.github.com>
2023-08-20 03:02:05 +0300

    comm

--- css/js/01-gallery.js
+++ css/js/01-gallery.js
@@ -2,3 +2,69 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const listEl = document.querySelector(".gallery");
+
+const renderList = (arr, container) => {
+    const markup = arr
+    .map(
+        (item) => `<li class="gallery__item">
+        <a class="gallery__link" href="large-image.jpg">
+          <img
+            class="gallery__image"
+            src="small-image.jpg"
+            data-source="large-image.jpg"
+            alt="Image description"
+          />
+        </a>
+      </li>`
+    )
+    .join("");
+
+    container.insertAdjacentHTML("beforeend", markup);
+};
+
+const handleListClick = (event) => {
+if (event.currentTurget === event.target) {
+    return;
+};
+
+const currentListItem = event.target.closest(".gallery__item");
+
+const instanceModal = basicLightbox.create(`
+    <div class="modal">
+        <p>
+            Your first lightbox with just a few lines of code.
+            Yes, it's really that simple.
+        </p>
+    </div>
+`)
+
+instanceModal.show()
+}
+
+renderList(galleryItems, listEl);
+listEl.addEventListener("click", handleListClick);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,22 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const listEl = document.querySelector(".gallery");
+
+const renderList = (arr, container) => {
+    const markup = arr.map((item) => `<li class="gallery_item">
+    <a class="gallery_link" href="${item.original}">
+    <img
+    class="gallery_image"
+    src="${item.previev}"
+    alt="${item.description}"
+    width="360"
+    />
+    </a>
+    </li>`).join("");
+
+    container.insertAdjacentHTML("afterbegin", markup);
+}
+
+renderList(galleryItems, listEl);
